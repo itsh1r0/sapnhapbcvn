423e9f23774fe971cccd1d1bdfc139d3537f1285
ItsukaHiro <itsukahiro.personal@proton.me>
2026-06-18 11:33:34 +0700

    Combine sites for project

---
+++ 360/data.js
@@ -0,0 +1,242 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "01",
+      "name": "Cổng Báo và Phát Thanh, Truyền hình Tuyên Quang",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.22045006592780325,
+        "pitch": -0.32675696397000564,
+        "fov": 1.489925106613882
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.5725703395853916,
+          "pitch": -0.7387728414740664,
+          "title": "Báo và Phát thanh Truyền hình Tuyên Quang",
+          "text": "Cơ quan báo chí, truyền thông chủ lực của tỉnh; nơi truyền tải kịp thời, chính thống mọi thông tin thời sự, chính trị, kinh tế và văn hóa của địa phương.",
+          "click": "sub-info/01_1.html"
+        }
+      ]
+    },
+    {
+      "id": "02",
+      "name": "Phòng Kỹ thuật trường quay",
+      "tiltCorrection": -0.0523,
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.201954556334449,
+        "pitch": -0.018792560341832854,
+        "fov": 1.489925106613882,
+        "roll": 0,
+        
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3904701423760226,
+          "pitch": 0.07821211019729724,
+          "rotation": 0,
+          "target": "03"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.2043745891274589,
+          "pitch": -0.2017241287546876,
+          "title": "Phòng Kỹ thuật trường quay",
+          "text": "Nơi vận hành toàn bộ hệ thống âm thanh, ánh sáng, kỹ thuật hình ảnh và điều khiển sóng. Đây là \"đầu não\" đảm bảo mỗi khung hình, phát ngôn được truyền tải đến khán giả một cách hoàn hảo, chuẩn xác nhất.",
+          "click": "sub-info/02_1.html"
+        }
+      ]
+    },
+    {
+      "id": "03",
+      "name": "Trường quay 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.257947302466653,
+          "pitch": 0.024484693369140942,
+          "rotation": 0,
+          "target": "02"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2920216367851278,
+          "pitch": 0.25638299442873524,
+          "title": "Trường quay 03",
+          "text": "Không gian ghi hình hiện đại, đa năng, chuyên dụng cho các chương trình thời sự, tọa đàm và chuyên mục chuyên sâu của Đài. Nơi ghi dấu sự chuyên nghiệp và kết nối trực quan giữa phát thanh viên với khán giả.",
+          "click": "sub-info/03_1.html"
+        }
+      ]
+    },
+    {
+      "id": "04",
+      "name": "Phòng Kỹ thuật sản xuất Chương trình truyền hình",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.2617429510693938,
+        "pitch": 0.12026737145242272,
+        "fov": 1.489925106613882
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.8125962617226943,
+          "pitch": -0.15927574963118296,
+          "title": "Phòng Kỹ thuật sản xuất Chương trình truyền hình",
+          "text": "Không gian hậu kỳ sáng tạo – nơi các kỹ thuật viên và biên tập viên thực hiện kỹ xảo, dựng phim, và trau chuốt âm thanh để biến những thước phim thô thành các sản phẩm truyền hình hoàn chỉnh, hấp dẫn.",
+          "click": "sub-info/04_1.html"
+        }
+      ]
+    },
+    {
+      "id": "05",
+      "name": "Văn phòng",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.7626295673270356,
+        "pitch": 0.17584519155591138,
+        "fov": 1.3507253861288488
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 3.136875854602014,
+          "pitch": -0.07834848872924027,
+          "title": "Văn Phòng",
+          "text": "placeholder"
+        }
+      ]
+    }
+  ],
+  "name": "360 Báo và PTTH Tuyên Quang",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
