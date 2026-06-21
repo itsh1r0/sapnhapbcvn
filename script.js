d08370cf633c89939d79a9c7a7530d8198a62c9c
ItsukaHiro <itsukahiro.personal@proton.me>
2026-06-21 13:00:22 +0700

    update viewpoint

--- script.js
+++ script.js
@@ -6,7 +6,17 @@ function doHijackZoom() {
     const viewWidth = window.innerWidth;
     const viewHeight = window.innerHeight;
 
-    let scaleRatio = viewWidth / BASE_WIDTH;
+    let baseWidth;
+
+    if (viewWidth > 1000) {
+        baseWidth = 1920;
+    } else if (viewWidth >= 721 && viewWidth <= 1000) {
+        baseWidth = 768;
+    } else {
+        baseWidth = 390;
+    }
+
+    let scaleRatio = viewWidth / baseWidth;
 
     iframe.style.width = (viewWidth / scaleRatio) + 'px';
     iframe.style.height = (viewHeight / scaleRatio) + 'px';
